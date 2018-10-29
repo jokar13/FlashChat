45e1e33ab9e46a5eb15f9198905fbd2a11d090d6
jokar13 <wivekks@gmail.com>
2018-10-30 00:58:41 +0530

    Retrieve the Locally Stored Data

--- app/src/main/java/com/jokar/flashchat/MainChatActivity.java
+++ app/src/main/java/com/jokar/flashchat/MainChatActivity.java
@@ -29,7 +29,7 @@ public class MainChatActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main_chat);
 
         // TODO: Set up the display name and get the Firebase reference
-
+        setupDisplayName();
 
         // Link the Views in the layout to the Java code
         mInputText = (EditText) findViewById(R.id.messageInput);
@@ -44,6 +44,14 @@ public class MainChatActivity extends AppCompatActivity {
     }
 
     // TODO: Retrieve the display name from the Shared Preferences
+    private void setupDisplayName(){
+
+        SharedPreferences prefs = getSharedPreferences(RegisterActivity.CHAT_PREFS, MODE_PRIVATE);
+
+        mDisplayName = prefs.getString(RegisterActivity.DISPLAY_NAME_KEY, null);
+
+        if (mDisplayName == null) mDisplayName = "Anonymous";
+    }
 
 
     private void sendMessage() {
